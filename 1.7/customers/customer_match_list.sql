a2ce3188c58b1e1cec29fe1fbaa01e5ee36d943b
Lorenzo Minciotti <lorenzo.minciotti@novanta.it>
2023-02-15 11:28:43 +0100

    Added customer match export

---
+++ 1.7/customers/customer_match_list.sql
@@ -0,0 +1,113 @@
+use borsellini;
+
+/*
+ * ==== CUSTOMER MATCH
+ * 1. Query to export all customer that have buyed something
+ * 2. Query to export all customer that not buy nothing but registerd
+ * 3. Query to export all non business customer
+ * 2. Query to export all business customer
+ */
+
+select 
+	c.firstname as `First Name`,
+    c.lastname as `Last Name`,
+    IFNULL(co.iso_code, '') as `Country`,
+    IFNULL(a.postcode, '') as `Zip`,
+    c.email as `Email`,
+	IFNULL(IF(a.phone like '0039%' or a.phone like '_0039%' or a.phone like '+39%', REPLACE(' ', '', REPLACE(a.phone, '0039', '+39')), CONCAT('+39', a.phone)), '') as `Phone`
+from ps_customer c
+left outer join ps_address a
+	on c.id_customer = a.id_customer
+    and lower(a.firstname) = lower(c.firstname)
+	and lower(a.lastname) = lower(c.lastname)
+left outer join ps_country co
+	on a.id_country = co.id_country
+left outer join ps_orders o
+	on o.id_customer = c.id_customer
+where o.id_order is not null
+group by c.email;
+
+
+select 
+	c.firstname as `First Name`,
+    c.lastname as `Last Name`,
+    IFNULL(co.iso_code, '') as `Country`,
+    IFNULL(a.postcode, '') as `Zip`,
+    c.email as `Email`,
+	IFNULL(IF(a.phone like '0039%' or a.phone like '_0039%' or a.phone like '+39%', REPLACE(' ', '', REPLACE(a.phone, '0039', '+39')), CONCAT('+39', a.phone)), '') as `Phone`
+from ps_customer c
+left outer join ps_address a
+	on c.id_customer = a.id_customer
+    and lower(a.firstname) = lower(c.firstname)
+	and lower(a.lastname) = lower(c.lastname)
+left outer join ps_country co
+	on a.id_country = co.id_country
+left outer join ps_orders o
+	on o.id_customer = c.id_customer
+where o.id_order is null
+group by c.email;
+
+
+
+select 
+	a.firstname as `First Name`,
+    a.lastname as `Last Name`,
+    IFNULL(co.iso_code, '') as `Country`,
+    IFNULL(a.postcode, '') as `Zip`,
+    c.email as `Email`,  
+    IF(
+		a.phone is not null and a.phone <> '', 
+		IF(a.phone like '0039%' or a.phone like '_0039%' or a.phone like '+39%', REPLACE(' ', '', REPLACE(a.phone, '0039', '+39')), CONCAT('+39', a.phone)), 
+        ''
+	) as `Phone`
+from ps_address a
+left outer join ps_country co
+	on a.id_country = co.id_country
+left outer join ps_customer c
+	on c.id_customer = a.id_customer
+where a.company is not null and a.company <> ''
+group by c.id_customer;
+
+
+select 
+	a.firstname as `First Name`,
+    a.lastname as `Last Name`,
+    IFNULL(co.iso_code, '') as `Country`,
+    IFNULL(a.postcode, '') as `Zip`,
+    c.email as `Email`,  
+    IF(
+		a.phone is not null and a.phone <> '', 
+		IF(a.phone like '0039%' or a.phone like '_0039%' or a.phone like '+39%', REPLACE(' ', '', REPLACE(a.phone, '0039', '+39')), CONCAT('+39', a.phone)), 
+        ''
+	) as `Phone`
+from ps_address a
+left outer join ps_country co
+	on a.id_country = co.id_country
+left outer join ps_customer c
+	on c.id_customer = a.id_customer
+where 
+	(a.company = '' or a.company is null) and 
+    a.`id_customer` not in (
+		select 
+			c_in.id_customer as `id_customer`
+		from ps_address a_in
+		left outer join ps_customer c_in
+			on c_in.id_customer = a_in.id_customer
+		where a_in.company is not null and a_in.company <> '' and c_in.id_customer is not null
+		group by c_in.id_customer
+	)
+group by c.id_customer
+
+union all
+
+select 
+	c.firstname as `First Name`,
+    c.lastname as `Last Name`,
+    '' as `Country`,
+    '' as `Zip`,
+    c.email as `Email`,  
+    '' as `Phone`
+from ps_customer c
+left outer join ps_address a
+	on c.id_customer = a.id_customer
+where a.id_customer is null;
